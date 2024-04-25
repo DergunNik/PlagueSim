520633fdb9614ced42eacd9e2fc26d76a895992e
Dziarhun Mikalai <dergunnik@gmail.com>
2024-04-25 15:21:33 +0300

    исправление main.cpp

--- main.cpp
+++ main.cpp
@@ -10,19 +10,5 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     WindowsManager windowsManager;
-
-    // QMainWindow window;
-
-    // // Создайте новое меню
-    // QMenu *fileMenu = new QMenu("&File");
-
-    // // Добавьте действия в меню
-    // fileMenu->addAction("&Save");
-
-    // // Добавьте меню в строку меню
-    // window.menuBar()->addMenu(fileMenu);
-    // window.show();
-
-
     return a.exec();
 }
